1ecfdc02771b453418f66827be0100b58464067f
pokohide <hyde31415@gmail.com>
2016-11-01 03:41:39 +0900

    app定義

---
+++ app.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const koa = require('koa')
+const cors = require('kcors')
+const app = koa()
+const port = process.env.PORT || 8080
+
+const origins = {
+  origin: '*',
+  resource: '*',
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'Origin', 'X-Requested-With']
+}
+
+// AVOID CROSS DOMEIN ISSUE
+app.use(cors(origins))
+
+// Run Server
+app.listen(port, () => {
+  console.log(`Server is listening at port ${port}`)
+})
+
+// Catch Error
+app.on('erorr', (err) => {
+  log.error('server error', err)
+})
+
+app.use(function* (next) {
+  const start = new Date
+  yield next
+  const ms = new Date - start
+  console.log(`${this.method} ${this.url} - ${ms}`)
+})
+
+// define route
+const router = require('./api/routes')(app)
