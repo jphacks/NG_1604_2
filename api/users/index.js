7013f98a576d7b5655b3ba245b9e27e6e09205d8
pokohide <hyde31415@gmail.com>
2016-11-01 03:32:58 +0900

    usersAPIを作詞絵

---
+++ api/users/index.js
@@ -0,0 +1,56 @@
+'use strict'
+
+const fb = require('../../lib/firebase')
+const q = require('q')
+
+/*
+* GET /api/users/fetch
+* userID
+*/
+const fetch = function* (next) {
+  const deferred = q.defer()
+  const userID = this.params.user_id
+
+
+  this.body = yield deferred.promise
+}
+
+/*
+* POST /api/users
+*
+*/
+const update = function* (next) {
+  const deferred = q.defer()
+  const userID = this.request.body.user_id
+
+  // update process
+  deferred.resolve({
+    message: 'ok'
+  })
+  // deferred.reject({ message: err })
+
+  this.body = yield deferred.promise
+}
+
+/*
+* POST /api/users/match
+*
+*/
+const match = function* (next) {
+  const deferred = q.defer()
+  const userID = this.request.body.user_id
+
+  // match process
+  deferred.resolve({
+    message: 'ok'
+  })
+  // deferred.reject({ message: err })
+
+  this.body = yield deferred.promise 
+}
+
+module.exports = {
+  fetch: fetch,
+  update: update,
+  match: match
+}
