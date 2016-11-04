f3bdd2d85e0141ce50840f52fb051460f56ea049
pokohide <hyde31415@gmail.com>
2016-11-04 23:53:46 +0900

    define like model

--- app/models/likes.js
+++ app/models/likes.js
@@ -1,21 +1,14 @@
 'use strict'
 
-/* ちゃんとバリデートもする */
 const mongoose = require('mongoose')
+const moment = require('moment')
 
-const Schema = mongoose.Schema
-const UserSchema = new Schema({
+const Schema = mongoose.Schama
+const LikeSchema = new Schema({
   uuid:       {type: String, required: true},
-  name:       {type: String, required: true},
-  gender:     {type: String, required: true},
-  univ:       {type: String, required: true},
-  thumb:      {type: String, required: true},
-  department: {type: String, required: true}
+  like_to:    {type: String, required: true},
+  is_match:   {type: Boolean, default: false},
+  createdAt:  {type: Date, default: moment()}
 })
 
-module.exports = mongoose.model('User', UserSchema)
-
-'use strict'
-
-const mongoose = require('mongoose')
-
+module.exports = mongoose.model('Like', LikeSchema)
