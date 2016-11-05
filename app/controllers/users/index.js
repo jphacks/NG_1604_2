bd003bcb8a5fb264f83e8bb48db4d8adc45c24fe
pokohide <hyde31415@gmail.com>
2016-11-06 00:37:11 +0900

    dummy recommends

--- app/controllers/users/index.js
+++ app/controllers/users/index.js
@@ -36,34 +36,18 @@ const show = function* (next) {
   promise.then((user) => {
     console.log(user)
     const recommend = user.recommends.split(',')
-    const recommends = recommend.map((elem) => {
-      return {uuid: elem}
-    })
+    //const recommends = recommend.map((elem) => {
+    //  return {uuid: elem}
+    //})
+    const recommends = [{uuid: '123456789'}, {uuid: '14141414'}, {uuid: '1313131313'}, {uuid: '121212121212'}]
     const promise2 = User.find({ '$or': recommends }).exec()
     promise2.then((users) => {
       console.log(users)
-    })
-    const u = {
-      "name": "村田雄介",
-      "gender": "男性",
-      "univ_name": "名古屋大学",
-      "univ_id": "meijyo",
-      "department": "理工学部",
-      "grade": 4,
-      "profile": "アジア各国の英語教育について考えています。使い古したスマートフォンで英語教育をアジア各国に届けます。",
-      "profile_img": "https://pbs.twimg.com/profile_images/378800000730413028/f4285b8685a7b6c95546ed3c5cdb247c_400x400.jpeg",
-      "classes": {
-        "mon": "1,0,0,1,1",
-        "tue": "1,0,0,1,1",
-        "wed": "0,0,0,0,0",
-        "thu": "0,1,1,1,0",
-        "fri": "0,0,0,0,1"
-      }
-    }
-    deferred.resolve({
-      user: u,
-      recommends: [u, u, u, u, u],
-      message: 'show'
+      deferred.resolve({
+        user: user,
+        recommends: users,
+        message: 'show'
+      })
     })
   })
   this.body = yield deferred.promise
@@ -102,9 +86,7 @@ const update = function* (next) {
   const deferred = q.defer()
   const uuid = this.params.uuid
   const body = this.request.body
-  //
-  console.log(this.request.body)
-  const promise = User.update({ uuid: uuid }, { $set: body }, { upsert: true, multi: false }).exec()
+  const promise = User.update({ uuid: uuid }, { $set: body }, { upsert: false, multi: false }).exec()
   promise.then(() => {
     analyze.calculateTF(uuid)
     deferred.resolve({
